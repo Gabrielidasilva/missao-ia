afec54943758d0ab5c13086287e8aad7ef8e2262
Gabrielidasilva <140640331+Gabrielidasilva@users.noreply.github.com>
2025-10-22 08:07:33 -0300

    1234

--- java/script.js
+++ java/script.js
@@ -1,3 +1,8 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-aternativas");
+const caixaResultados = document.querySelector(".caixa-resultados");
+const textoResultado = document.querySelector(".texto-resultado");
+const botaoJogarNovamente= document.querySelector(".novamente-botao");
+const telaInicial = document.querySelector(".tela-inicial");
+const botaoIniciar = document.querySelector(".iniciar-botao");
