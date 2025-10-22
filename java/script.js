0407c10a1a66588eb4b07e07dd7018cba9a4d650
Gabrielidasilva <140640331+Gabrielidasilva@users.noreply.github.com>
2025-10-22 08:12:09 -0300

    1234

--- java/script.js
+++ java/script.js
@@ -6,3 +6,9 @@ const textoResultado = document.querySelector(".texto-resultado");
 const botaoJogarNovamente= document.querySelector(".novamente-botao");
 const telaInicial = document.querySelector(".tela-inicial");
 const botaoIniciar = document.querySelector(".iniciar-botao");
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+botaoIniciar.addEventListener('click,iniciaJogo');
