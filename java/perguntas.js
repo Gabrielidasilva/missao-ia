13cd995004c62aaa2d75a41ae97c488c2a80610f
Gabrielidasilva <140640331+Gabrielidasilva@users.noreply.github.com>
2025-11-03 09:47:12 -0300

    1234

---
+++ java/perguntas.js
@@ -0,0 +1,9 @@
+export const perguntas = [
+    {}
+    enunciado:"A trajetória atual projeta que, até 2050, a concentração de riqueza atingirá níveis inéditos, enquanto a crise climática ultrapassará o limite de $1.5^\circ\text{C}$ de aquecimento. A métrica dominante de sucesso, o Produto Interno Bruto (PIB), ignora o esgotamento do capital natural e o aumento das disparidades sociais."
+    alternativas:[
+        {
+            texto
+        }
+    ]
+]
